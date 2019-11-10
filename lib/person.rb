e0acf9f4d3f6a2ee2e9ade60e7eebacfd4b47df5
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-10 19:54:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,13 @@
 class Person
-  def intialize (persons_name)
-    @name = persons_name
-  end
+  ef initialize (persons_name)
+      @name = persons_name
+    end
 
-  def name = (persons_name)
-  @name = persons_name
-end
+    def name=(persons_name)
+      @name = persons_name
+    end
 
-def name
-  @name
-
-end
+    def name
+      @name
+    end
+    
